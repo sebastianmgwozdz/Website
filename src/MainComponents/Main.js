65173d1d16e9c542fab45e7e8380ecd18f8d5588
Sebastian Gwozdz <sebastiangwozdz@Sebastians-MacBook-Pro.local>
2020-01-07 15:08:19 -0800

    reverted auth

--- src/MainComponents/Main.js
+++ src/MainComponents/Main.js
@@ -4,19 +4,7 @@ import About from "./About";
 import Education from "./Education";
 import Projects from "./Projects";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import TypingGame from "../TypingGameComponents/App";
-import PeerCoding from "../PeerCodingComponents/App";
-import { Auth0Provider } from "../PeerCodingComponents/react-auth0-spa";
-import config from "../auth_config.json";
-import history from "../utils/history";
-
-const onRedirectCallback = appState => {
-  history.push(
-    appState && appState.targetUrl
-      ? appState.targetUrl
-      : window.location.pathname
-  );
-};
+import App from "../TypingGameComponents/App";
 
 export default function Main() {
   function setPage(path) {
@@ -33,17 +21,7 @@ export default function Main() {
           <Projects switchPage={setPage}></Projects>
         </Route>
         <Route exact path="/typingtest">
-          <TypingGame switchPage={setPage}></TypingGame>
-        </Route>
-        <Route exact path="/peercoding/login">
-          <Auth0Provider
-            domain={config.domain}
-            client_id={config.clientId}
-            redirect_uri={"https://sebastiangwo.com/peercoding/"}
-            onRedirectCallback={onRedirectCallback}
-          >
-            <PeerCoding></PeerCoding>
-          </Auth0Provider>
+          <App switchPage={setPage}></App>
         </Route>
       </Switch>
     </Router>
--- src/PeerCodingComponents/Login.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { useAuth0 } from "./react-auth0-spa";
-import NavBar from "./NavBar";
-
-export default function Login() {
-  return <NavBar></NavBar>;
-}
