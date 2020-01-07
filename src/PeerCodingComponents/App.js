503f08d8eb6e0a6fbef97570dbd7541abe1f2b66
Sebastian Gwozdz <sebastiangwozdz@Sebastians-MacBook-Pro.local>
2020-01-07 15:05:28 -0800

    t

--- src/PeerCodingComponents/App.js
+++ src/PeerCodingComponents/App.js
@@ -1,6 +1,9 @@
 import React from "react";
-import NavBar from "./NavBar";
+import Login from "./NavBar";
 import { useAuth0 } from "./react-auth0-spa";
+import { Router, Route, Switch } from "react-router-dom";
+import Profile from "./Profile";
+import history from "../utils/history";
 
 export default function App() {
   const { loading } = useAuth0();
@@ -11,9 +14,15 @@ export default function App() {
 
   return (
     <div className="App">
-      <header>
-        <NavBar />
-      </header>
+      {/* Don't forget to include the history module */}
+      <Router history={history}>
+        <header>
+          <Login />
+        </header>
+        <Switch>
+          <Route path="/peercoding" component={Profile} />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/PeerCodingComponents/Login.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { useAuth0 } from "./react-auth0-spa";
+import NavBar from "./NavBar";
+
+export default function Login() {
+  return <NavBar></NavBar>;
+}
