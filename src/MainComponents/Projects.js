75654af463ff59f27d286cf830ea1f657d73e488
Sebastian Gwozdz <sebastiangwozdz@Sebastians-MacBook-Pro.local>
2020-01-17 18:01:39 -0800

    fixed urls

--- src/MainComponents/Projects.js
+++ src/MainComponents/Projects.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React from "react";
 import "./css/Projects.css";
 import ProjectTable from "./ProjectTable";
 import ProjectCard from "./ProjectCard";
