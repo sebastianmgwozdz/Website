7dce8654a43edab8325d6952ab6e0714fb91157b
Sebastian Gwozdz <sebastiangwozdz@Sebastians-MacBook-Pro.local>
2020-06-19 12:50:24 -0700

    fixed ssl issues

--- src/links.js
+++ src/links.js
@@ -1,5 +1,5 @@
-let main = "https://sebastiangwo.com/";
+let main = "http://localhost:3000/";
 let papertrader = main + "papertrader/home";
-let server = "http://159.65.69.250/";
+let server = "https://159.65.69.250/";
 
 export { main, papertrader, server };
