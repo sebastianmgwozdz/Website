dc31b0f9024b6abe31fd89116cd1cc2466db16e0
Sebastian Gwozdz <sebastiangwozdz@Sebastians-MacBook-Pro.local>
2020-07-03 19:11:16 -0700

    updated projects section to feature cards

--- src/MainComponents/ProjectCard.js
+++ src/MainComponents/ProjectCard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
@@ -7,31 +7,50 @@ import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Modal } from "antd";
 
 const useStyles = makeStyles({
   card: {
     maxWidth: 300,
-    maxHeight: 350
+    maxHeight: 350,
   },
   media: {
-    height: 100
-  }
+    height: 100,
+  },
 });
 
 export default function ProjectCard(props) {
-  const { image, title, text, switchFunc } = props;
+  const { switchFunc, data } = props;
   const classes = useStyles();
+
+  const [open, setOpen] = useState(false);
 
   return (
     <div style={{ marginLeft: "2%" }}>
+      <Modal
+        title={data["title"]}
+        visible={open}
+        onOk={() => {
+          setOpen(false);
+        }}
+        onCancel={() => {
+          setOpen(false);
+        }}
+      >
+        <p>{data["video"]}</p>
+        <p>{data["features"]}</p>
+        <p>{data["function"]}</p>
+        <p>{data["tools"]}</p>
+        <p>{data["demo"]}</p>
+      </Modal>
       <Card className={classes.card}>
-        <CardMedia className={classes.media} image={image} />
+        <CardMedia className={classes.media} image={data["image"]} />
         <CardContent>
           <Typography gutterBottom variant="h5" component="h2">
-            {title}
+            {data["title"]}
           </Typography>
           <Typography variant="body2" color="textSecondary" component="p">
-            {text}
+            {data["description"]}
           </Typography>
         </CardContent>
         <CardActions>
@@ -40,10 +59,10 @@ export default function ProjectCard(props) {
               size="small"
               color="primary"
               onClick={() => {
-                switchFunc();
+                setOpen(true);
               }}
             >
-              Demo
+              More Info
             </Button>
           </Router>
         </CardActions>
--- src/MainComponents/Projects.js
+++ src/MainComponents/Projects.js
@@ -4,21 +4,71 @@ import ProjectTable from "./ProjectTable";
 import ProjectCard from "./ProjectCard";
 import { withRouter } from "react-router-dom";
 
-function Projects(props) {
+let projects = [
+  {
+    title: "Paper Trader",
+    description:
+      "Platform for users to track their favorite stocks and make trades with paper money",
+    image: require("./images/test.jpeg"),
+    video: "video",
+    features: ["feature 1, feature 2"],
+    function: "function",
+    tools: "tools",
+    demo: "demo",
+  },
+  {
+    title: "Typing Test",
+    description:
+      "A lightweight application that provides user with feedback regarding typing ability",
+    image: require("./images/test.jpeg"),
+    video: "video",
+    features: ["feature 1, feature 2"],
+    function: "function",
+    tools: "tools",
+    demo: "demo",
+  },
+  {
+    title: "Shortest Path Visualizer",
+    description:
+      "Visualization of the shortest possible path between two specified points in a 2D grid",
+    image: require("./images/test.jpeg"),
+    video: "video",
+    features: ["feature 1, feature 2"],
+    function: "function",
+    tools: "tools",
+    demo: "demo",
+  },
+  {
+    title: "Evasive Maneuvers",
+    description:
+      "2D Game inspired by Helicopter Game and Flappy Bird where the user maneuvers through a series of obstacles",
+    image: require("./images/test.jpeg"),
+    video: "video",
+    features: ["feature 1, feature 2"],
+    function: "function",
+    tools: "tools",
+    demo: "demo",
+  },
+];
+
+function cards() {
+  console.log(
+    projects.map((val) => {
+      return <ProjectCard data={val}></ProjectCard>;
+    })
+  );
+  return projects.map((val) => {
+    return <ProjectCard data={val}></ProjectCard>;
+  });
+}
+
+function Projects() {
   return (
     <div className="Projects-Background" id="projects">
       <header className="Projects-Header">Projects</header>
-      <div className="w-50 p-3" style={{ display: "flex", marginLeft: "2%" }}>
-        <ProjectTable />
-        <ProjectCard
-          title="Typing Test"
-          text="Application where the user can test their typing speed and accuracy in real-time."
-          image={require("./images/test.jpeg")}
-          switchFunc={() => {
-            props.history.push("/typingtest");
-          }}
-        ></ProjectCard>
-      </div>
+      <span className="w-50 p-3" style={{ display: "flex", marginLeft: "2%" }}>
+        {cards()}
+      </span>
     </div>
   );
 }
