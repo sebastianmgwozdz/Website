b78f7b3d4ee51427b3f85c03f601d835c1dbe7e0
Sebastian Gwozdz <sebastiangwozdz@Sebastians-MacBook-Pro.local>
2020-07-14 11:35:11 -0700

    added badges and tabs to papertrader earnings calendar

--- src/PaperTraderComponents/CalendarComponents/EarningsCalendar.js
+++ src/PaperTraderComponents/CalendarComponents/EarningsCalendar.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { Calendar, Select, Col, Row, Typography, Badge, Card } from "antd";
+import { Card, Tabs, Badge } from "antd";
 import { get } from "../Helpers";
+
+const { TabPane } = Tabs;
 
 export default function EarningsCalendar() {
   const [earnings, setEarnings] = useState(undefined);
@@ -8,6 +10,8 @@ export default function EarningsCalendar() {
   useEffect(() => {
     let start = Date.now();
     let end = start + 2628000000;
+
+    console.log(new Date(start));
 
     let startForm = new Date(start).toISOString().substring(0, 10);
     let endForm = new Date(end).toISOString().substring(0, 10);
@@ -39,31 +43,67 @@ export default function EarningsCalendar() {
     return m;
   }
 
-  function symbolList(earnings) {}
+  function getBadgeColor(value) {
+    let hour = value["hour"];
 
-  function getListData() {
+    switch (hour) {
+      case "bmo":
+        return "red";
+      case "dmh":
+        return "green";
+      default:
+        return "blue";
+    }
+  }
+
+  function getListData(date) {
+    let arr = earnings.get(date);
+
+    let data = arr.map((val) => {
+      return (
+        <div>
+          <Badge color={getBadgeColor(val)} text={val["symbol"]} />
+        </div>
+      );
+    });
+
+    return <ul>{data}</ul>;
+  }
+
+  function getTabs() {
     if (!earnings) {
       return [];
     }
 
-    let data = [];
+    let t = [];
 
-    for (let d of earnings.keys()) {
-      let e = earnings.get(d).map((val) => {
-        return <li>{val["symbol"]}</li>;
-      });
+    let arr = Array.from(earnings.keys()).sort((a, b) => {
+      return a.localeCompare(b);
+    });
 
-      data.push(
-        <Card title={d} style={{ width: 300 }}>
-          {<ul>{e}</ul>}
-        </Card>
+    console.log(arr);
+
+    for (let i = 0; i < arr.length; i++) {
+      let date = arr[i];
+
+      t.push(
+        <TabPane tab={date} key={i}>
+          {getListData(date)}
+        </TabPane>
       );
     }
 
-    console.log(data);
-
-    return data.reverse();
+    return t;
   }
 
-  return <div>{getListData()}</div>;
+  //<Badge color={"red"} text={"Test"} />
+
+  return (
+    <header
+      style={{ width: "100%", display: "flex", justifyContent: "center" }}
+    >
+      <Tabs defaultActiveKey="1">{getTabs()}</Tabs>
+    </header>
+  );
+  //return <div>{getListData()}</div>;
 }
