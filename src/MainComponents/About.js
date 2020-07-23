c27058fea8c88b40d6cbddddf2c3d2f6424c5dbd
Sebastian Gwozdz <sebastiangwozdz@Sebastians-MacBook-Pro.local>
2020-07-22 19:57:23 -0700

    redesigned site structure and individual sections

--- src/MainComponents/About.js
+++ src/MainComponents/About.js
@@ -7,25 +7,12 @@ import Divider from "@material-ui/core/Divider";
 import ListItemText from "@material-ui/core/ListItemText";
 import ListItemAvatar from "@material-ui/core/ListItemAvatar";
 import Avatar from "@material-ui/core/Avatar";
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    width: "100%",
-    maxWidth: 360,
-    backgroundColor: "gray",
-  },
-  inline: {
-    display: "inline",
-  },
-}));
+import Skills from "./Skills";
 
 export default function About() {
-  const classes = useStyles();
-
   return (
     <div className="About-Background" id="about">
       <header className="About-Header">
-        About Me
         <span className="About-Subtext">
           From an early age, I have always had a place in my heart for
           technology. I appreciate tech in all forms, from cars to smartphones,
@@ -35,56 +22,7 @@ export default function About() {
           Programming is one of the many tools I use to achieve that.
         </span>
         <span style={{ marginBottom: "2%", marginTop: "2%" }}>Skills</span>
-        <div>
-          <div style={{ float: "left" }}>
-            <List className={classes.root}>
-              <ListItem alignItems="flex-start">
-                <ListItemAvatar>
-                  <Avatar alt="Java" src="icons/java.jpg" />
-                </ListItemAvatar>
-                <ListItemText primary="Java" />
-              </ListItem>
-              <Divider variant="inset" component="li" />
-              <ListItem alignItems="flex-start">
-                <ListItemAvatar>
-                  <Avatar alt="JavaScript" src="icons/javascript.png" />
-                </ListItemAvatar>
-                <ListItemText primary="JavaScript" />
-              </ListItem>
-              <Divider variant="inset" component="li" />
-              <ListItem alignItems="flex-start">
-                <ListItemAvatar>
-                  <Avatar alt="React" src="icons/react.png" />
-                </ListItemAvatar>
-                <ListItemText primary="React" />
-              </ListItem>
-            </List>
-          </div>
-          <div style={{ float: "left" }}>
-            <List className={classes.root}>
-              <ListItem alignItems="flex-start">
-                <ListItemAvatar>
-                  <Avatar alt="C#" src="icons/csharp.png" />
-                </ListItemAvatar>
-                <ListItemText primary="C#" />
-              </ListItem>
-              <Divider variant="inset" component="li" />
-              <ListItem alignItems="flex-start">
-                <ListItemAvatar>
-                  <Avatar alt="Python" src="icons/python.jpg" />
-                </ListItemAvatar>
-                <ListItemText primary="Python" />
-              </ListItem>
-              <Divider variant="inset" component="li" />
-              <ListItem alignItems="flex-start">
-                <ListItemAvatar>
-                  <Avatar alt="HTML" src="icons/html.png" />
-                </ListItemAvatar>
-                <ListItemText primary="HTML" />
-              </ListItem>
-            </List>
-          </div>
-        </div>
+        <Skills></Skills>
       </header>
     </div>
   );
--- src/MainComponents/Front.js
+++ src/MainComponents/Front.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import "./css/FrontPage.css";
-import NavBar from "./NavBar.js";
 import Loader from "../Loader";
 import { withRouter } from "react-router-dom";
 import { main } from "../links";
@@ -30,19 +29,19 @@ function Front(props) {
         style={{
           height: "100vh",
           zIndex: 0,
-          display: displayProp()
+          display: displayProp(),
         }}
       ></img>
       <div className="Transparent-Background">
         <header className="Bio-header">
           Sebastian Gwozdz
           <br />
-          <p className="Contact">sebastianmgwozdz@gmail.com</p>
+          <p className="Contact ">sebastianmgwozdz@gmail.com</p>
           <div className="Icon">
-            <a href="https://www.linkedin.com/in/sebastian-gwozdz-05355a16a/">
+            <a href="https://www.linkedin.com/in/sebastian-gwozdz/">
               <img
                 src="icons/linkedin.png"
-                height="75"
+                height="80"
                 width="80"
                 alt="LinkedIn Icon"
                 className="Image"
@@ -51,14 +50,13 @@ function Front(props) {
             <a href="https://github.com/sebastianmgwozdz">
               <img
                 src="icons/github.png"
-                height="75"
+                height="80"
                 width="80"
                 alt="Github Icon"
                 className="Image"
               />
             </a>
           </div>
-          <NavBar />
         </header>
       </div>
     </div>
